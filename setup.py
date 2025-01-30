291de895e9ed97d4c4b4327fbf12f1f2ca7c629f
saanjanaa17 <sanjanadjalva@gmail.com>
2025-01-30 06:05:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name = 'mlproject_regression',
     version= '0.0.1',
-    author='Utkarsh Gaikwad',
-    author_email='gaikwadujg@gmail.com',
+    author='Sanjana_Alva',
+    author_email='sanjanayalva@gmail.com',
     packages = find_packages(),
     install_requires = get_requirements('requirements.txt')
 )
